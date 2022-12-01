ee91ba0f0a4bc30c6b796d807b84bd069162bfb1
Lucas <61168687+LucasL1K3Melo@users.noreply.github.com>
2022-12-01 05:11:10 -0300

    Popup

---
+++ Popup/js/script.js
@@ -0,0 +1,13 @@
+console.log("All good!");
+
+const container = document.getElementById('container');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+
+open.addEventListener("click", () => {
+    container.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+    container.classList.remove("active");
+})
